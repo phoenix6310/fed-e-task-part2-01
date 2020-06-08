8d4ab68cdf582a29ef07e18504a3d95044513309
pengjie(彭杰) <peng.jie@founder.com.cn>
2020-06-08 18:05:28 +0800

    'update'

--- code/generator-demo/generators/app/index.js
+++ code/generator-demo/generators/app/index.js
@@ -1,22 +1,52 @@
 const Generator = require('yeoman-generator')
 
 module.exports = class extends Generator {
+    // 3
+    prompting(){
+        // 钩子函数，调用此模块时，自动调用
+        // prompt 为 父类 Generator 的方法
+        return this.prompt([
+            {
+                type: 'input',
+                name: 'projectName',
+                message: 'Your project name',
+                default: this.appname
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Your email',
+                default: ''
+            }
+        ]).then(res=>{
+            // { 'projectName': 'ss', email: '32@qq.com' }
+            this.promptRes = res
+        })
+    }
     writing(){
-        // 1
+        // // 1
         // this.fs.write(
         //     this.destinationPath('demo.txt'),
         //     "生成的内容"
         // )
         // 2
+        // // 模板文件路径
+        // const tmpPath = this.templatePath('foo.txt')
+        // // 输出目标路径
+        // const output = this.destinationPath('foo.txt')
+        // // 模板数据
+        // const context = {
+        //     title: 'hello, generator',
+        //     success: true
+        // }
+        // this.fs.copyTpl(tmpPath, output, context)
+        // 3
         // 模板文件路径
-        const tmpPath = this.templatePath('foo.txt')
+        const tmpPath = this.templatePath('index.html')
         // 输出目标路径
-        const output = this.destinationPath('foo.txt')
+        const output = this.destinationPath('index.html')
         // 模板数据
-        const context = {
-            title: 'hello, generator',
-            success: true
-        }
+        const context = this.promptRes
         this.fs.copyTpl(tmpPath, output, context)
     }
 }
